ea2eaa674d87026078bed0b85d904d46df8bea29
madhavisandur <madhavimsandur29@gmail.com>
2025-05-14 15:30:39 +0530

    Changes done on lang and header and content part
    Changes done on lang and header and content part

---
+++ ElPaisHomePage.java
@@ -0,0 +1,47 @@
+package mavenproject.elpaisProject;
+
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+
+public class ElPaisHomePage {
+
+     WebDriver driver;
+     String url = "https://elpais.com/";
+
+    // Locator for Language
+ 
+    private By Language = By.xpath("//meta[@name='lang']");
+
+
+   // Constructor
+    public ElPaisHomePage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+  
+
+// Return the boolean value if spanish is detected
+    public boolean isSpanish() {
+    	 
+        String htmlLang = driver.findElement(By.tagName("html")).getAttribute("lang");
+    	if(htmlLang.compareToIgnoreCase("es-ES")==0)
+    		return true;
+    	else
+    		return false;
+    	
+    }
+    
+    public String AssertSpanish() {
+   	 
+        String htmlLang = driver.findElement(By.tagName("html")).getAttribute("lang");
+    	return htmlLang;
+    	
+    }
+
+
+}
